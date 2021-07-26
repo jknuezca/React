392ef015bcd8881c773b3c16b4b7dfb6ce56d8bc
Kenneth <nuezcajorge@gmail.com>
2021-07-27 01:18:01 +0800

    Added styling

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,5 +1,6 @@
 import { div } from 'prelude-ls';
 import React from 'react';
+import './SeasonDisplay.css';
 
 const seasonConfig = {
   summer: {
@@ -7,7 +8,7 @@ const seasonConfig = {
     iconName: 'sun'
   },
   winter: {
-    text:'Burr, it is chilly.',
+    text:'Burr, it is chilly!',
     iconName:'snowflake'
   }  
 };
@@ -26,10 +27,10 @@ const SeasonDisplay = (props) => {
 
 
     return (
-        <div>
-            <i className={`${iconName} icon`} />
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
             <h1>{text}</h1>
-            <i className={`${iconName} icon`} />
+            <i className={`icon-right massive ${iconName} icon`} />
         </div>
     );
 
